72098df01b036f13cc31b8c454c04efa2aff84f4
sriramk7-hk <sriramkesavan44@gmail.com>
2023-03-17 14:16:02 +0530

    Final Frontend

--- frontend/src/pages/UploadFile.js
+++ frontend/src/pages/UploadFile.js
@@ -1,29 +1,63 @@
-import {  useEffect, useState } from "react";
-import axios, { Axios } from "axios";
+import { useState } from "react";
+import axios from "axios";
+import { Blob, NFTStorage } from "nft.storage";
+
+const nftapi = process.env.NFTSTORAGE_API_KEY
+
 
 export default function UploadFile() {
     const [selectedImage, setSelectImage] = useState("");
     const [viewImage, setViewImage] = useState("");
     const [text, setText] = useState("")
-
+    const [description, setDescription] = useState("");
+    const [tokenURI, setTokenURI] = useState("");
+    const [imageIPFS, setImageIPFS] = useState("");
+    let tokenURITemplate = {
+      name: "",
+      description: "",
+      image: "",
+    }
     const handleFiles = async () => {
       if(!selectedImage) return
       const formData = new FormData();
       formData.append("File", selectedImage, text.toString());
-      const data = await axios.post("/api/saveFile", formData);
-      console.log(data)
+      const res = await axios.post("/api/saveFile", formData);
+      console.log(res)
 
-      // await axios.get("api/saveFile").then((resp) => {
-      //   console.log(resp)
-      // })
+      let tokenURIMetaData = {...tokenURITemplate}
+      tokenURIMetaData.name = text;
+      tokenURIMetaData.description = description;
+      tokenURIMetaData.image = `ipfs://${res.data.IpfsHash}`;
+
+      let imageIPFSURL = `https://ipfs.io/ipfs/${res.data.IpfsHash}`
+
+      setImageIPFS(imageIPFSURL)
+
+      console.log(tokenURIMetaData);
+      //console.log(nftapi)
+
+
+      const client = new NFTStorage({token: nftapi});
+      const blob = new Blob([JSON.stringify(tokenURIMetaData)], {"type": "application/json"})
+      const data = await client.storeBlob(blob)
+
+      console.log(data);
       
+      let tokenURIURL = `https://ipfs.io/ipfs/${data}`;
+      console.log(tokenURIURL)
+      setTokenURI(tokenURIURL)
+
       
     }
     return(
         <div className="flex flex-col items-center p-10 space-y-6">
             <img src={viewImage} alt="Upload file within 100MB" height="100" width="100"></img>
+            
             <input type="text" onChange={(event) => {
                 setText(event.target.value);
+              }}/>
+              <input type="text" onChange={(event) => {
+                setDescription(event.target.value)
               }}/>
               <input type="file" name="SelectImage"  onChange={({target})=>{
               if(target.files){
@@ -32,6 +66,8 @@ export default function UploadFile() {
                 setSelectImage(file);
               }
               }} />
+              <a href={imageIPFS}>ImageLink</a>
+              <a href={tokenURI}>TokenMetaDatLink</a>
               <button onClick={handleFiles}>Upload</button>
         </div>
     )
